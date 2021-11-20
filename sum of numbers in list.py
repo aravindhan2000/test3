b0267488ea1ed8d8cb5f22389b309d8d65a8c187
aravindhan2000 <93928239+aravindhan2000@users.noreply.github.com>
2021-11-20 13:55:27 +0530

    sum of numbers

---
+++ sum of numbers in list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def sum(a):
+    result = 0
+    for i in a:
+        result+= i
+    return result
+print(sum((8, 2, 3, 0, 7)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
