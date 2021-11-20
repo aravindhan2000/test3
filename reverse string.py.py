14d776e7861b6b1d63cd0d880a275e508a6a75c0
aravindhan2000 <93928239+aravindhan2000@users.noreply.github.com>
2021-11-20 13:57:52 +0530

    reverse string

---
+++ reverse string.py.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[86]:
+
+
+def reverse_string(x):
+    return x[::-1]
+my_str=reverse_string('123abcd')
+print(my_str)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
